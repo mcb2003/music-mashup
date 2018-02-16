6b8f3aed2988d1b1b07ae5e77f853a34ee8c59a2
mcb2003 <mikeybuchan@hotmail.co.uk>
2018-02-16 14:51:36 +0000

    added boiler plate to music-mashup.py file, along with the shebang line.
    Signed-off-by: mcb2003 <mikeybuchan@hotmail.co.uk>

---
+++ music-mashup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python2
+#    Music MashUp    -    a program to generate fading audio files of song intros, usefull in music quizzes
+#    Copyright (C) 2018  Michael Connor Buchan
+#
+#    Music MashUp is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Music MashUp is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Music MashUp.  If not, see <http://www.gnu.org/licenses/>.
+
